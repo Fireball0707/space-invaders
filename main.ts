2867d29d45b442e38cac6169325df680adf7cf8f
Fireball0707 <67537426+Fireball0707@users.noreply.github.com>
2020-06-28 09:06:49 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    Spaceship.move(1)
+})
+input.onButtonPressed(Button.B, function () {
+    Spaceship.move(1)
+})
+let Spaceship: game.LedSprite = null
+Spaceship = game.createSprite(2, 4)
+basic.forever(function () {
+	
+})
